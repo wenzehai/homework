444da0d897ed525361740e582fbeafcc0543a1af
wenzehai_cool <yamuchashaoye@live.com>
2017-07-07 10:31:04 +0800

    增加了js第二章作业

---
+++ js-charpt2/作业2/scripts/script.js
@@ -0,0 +1,30 @@
+window.onload = function() {
+	var plusNum = document.getElementById('plusNum');
+	var addNum = document.getElementById('addNum');
+	var goodsNum = document.getElementById('goodsNum');
+	var singleAddedPrice = document.getElementById('singleAddedPrice');
+	var totalPrice = document.getElementById('totalPrice');
+	var MaximumPrice = document.getElementById('MaximumPrice');
+	
+	addNum.onclick = function(){
+		var singleNum = 0;
+			singleNum++;
+			goodsNum.innerHTML = singleNum;
+	}
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+};
